c3f07e101598b62fdfab8e4619549eccfe57855a
AlexFariaa <alexcaras1@hotmail.com>
2022-04-27 23:49:56 -0300

    connect database

---
+++ src/database/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  console.log("Conectando com o banco de dados");
+
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopoLogy: true,
+    })
+    .then(() => console.log("MongoDB Conectado!"))
+    .catch((err) => console.log(`erro ao conectar com o banco ${err}`));
+};
+
+module.exports = connectDatabase
