6388604d2efaf39f889777e087c93e30aa79b2e9
AlexFariaa <alexcaras1@hotmail.com>
2022-05-03 20:15:46 -0300

    auth arrumado

--- src/auth/auth.route.js
+++ src/auth/auth.route.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const authController = require("./auth.controller");
+const {validLogin} = require("./auth.middleware")
 
-router.post("/login", authController.loginController);
+router.post("/login",validLogin, authController.loginController);
 
 module.exports = router;
--- src/characters/route/characters.route.js
+++ src/characters/route/characters.route.js
@@ -5,33 +5,29 @@ const {
   validObjectBody,
 } = require("../../middlewares/characters.middlewares");
 
-const {validLogin} = require("../../auth/auth.middleware")
-
-router.get("/",validLogin, charactersController.findAllCharactersController);
+router.get("/", charactersController.findAllCharactersController);
 
 router.get(
   "/find/:id",
   validId,
-  validLogin,
   charactersController.findByIdCharacterController
 );
 router.post(
   "/create",
   validObjectBody,
-  validLogin,
   charactersController.createCharacterController
 );
 router.put(
   "/update/:id",
   validObjectBody,
   validId,
-  validLogin,
+
   charactersController.updateCharacterController
 );
 router.delete(
   "/delete/:id",
   validId,
-  validLogin,
+
   charactersController.deleteCharacterController
 );
 router.get("/search", charactersController.findByNameCharacterController);
